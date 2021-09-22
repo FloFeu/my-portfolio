0c16b27a892d93cc24bdf94084b094b64fc7d763
Florent Feugere <florent.feugere@gmail.com>
2021-09-22 16:35:00 +0200

    Adding some JS

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/**
+ * 
+   scroll to top button
+ */
 const toTop = document.getElementById('to-top')
 toTop.addEventListener('click', e => {
     e.preventDefault();
@@ -5,6 +9,32 @@ toTop.addEventListener('click', e => {
         top: 0,
         behavior: 'smooth'
     })
-}) ;
+});
 
 
+/**
+ * Adding border on nav links depending on what section we're on.   
+*/
+const home = document.getElementById('home');
+const projects = document.getElementById('projects');
+const skills = document.getElementById('skills');
+const interests = document.getElementById('interests');
+const resume = document.getElementById('resume')
+
+const test = [
+    home, projects, skills, interests, resume
+];
+
+const sectionObserver = new IntersectionObserver(function (entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            document.getElementById(`a_${entry.target.id}`).classList.add("active");
+        } else {
+            document.getElementById(`a_${entry.target.id}`).classList.remove("active");
+        }
+    })
+}, { threshold: 0.6 });
+
+test.forEach(i => {
+    sectionObserver.observe(i);
+});
